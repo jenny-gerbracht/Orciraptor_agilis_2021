69ea5fd385a262668fb416060dbda81c278a5d8a
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-03-09 10:27:32 +0100

    Update readprocessing_and_assembly.sh

--- Module_1/readprocessing_and_assembly.sh
+++ Module_1/readprocessing_and_assembly.sh
@@ -18,12 +18,12 @@ Discard_dir="/scratch4/shess/Jenny/Scripts/"
 date=$(date "+%Y-%m-%d")
 
 # Define log file and redirect stdout and stderr to this file
-if [ ! -d "${mydir}/Logs/" ]; then
+if [ ! -d "${moduledir}/Logs/" ]; then
   mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${mydir}/Logs/"
-  mkdir ${mydir}/Logs/
+  echo "$mytime Make directory ${moduledir}/Logs/"
+  mkdir ${moduledir}/Logs/
 fi
-log_file="${mydir}/Logs/log_$date"
+log_file="${moduledir}/Logs/log_$date"
 exec &> >(tee -a "$log_file")
 
 # Create necessary folders
@@ -62,12 +62,22 @@ ln -s $readdir/HI.3499.002.D704---D504.NA_R1.fastq ${moduledir}/readprocessing/N
 ln -s $readdir/HI.3499.002.D704---D504.NA_R2.fastq ${moduledir}/readprocessing/NA_R2.fastq
 
 # kmer-based error correction with Rcorrector
+echo ""
+echo "###################"
+echo "## Rcorrector"
+echo "###################"
+echo ""
 perl ${Rcorrector_dir}/run_rcorrector.pl \
   -p ${moduledir}/readprocessing/NA_R1.fastq ${moduledir}/readprocessing/NA_R2.fastq \
   -od ${moduledir}/readprocessing/corrected/ \
   -t 10
  
 # Remove unfixable read pairs, compress output
+echo ""
+echo "###################"
+echo "## Discard"
+echo "###################"
+echo ""
 python ${Discard_dir}/FilterUncorrectabledPEfastq.py \
 -1 ${moduledir}/readprocessing/corrected/NA_R1.cor.fastq \
 -2 ${moduledir}/readprocessing/corrected/NA_R2.cor.fastq \
@@ -77,6 +87,14 @@ pigz unfixrm_NA_R2.cor.fq
 
 # Perform quality and adapter trimming with Trim-Galore!
 source activate trim-galore
+echo ""
+echo "###################"
+echo "## Trim-Galore!"
+echo "###################"
+echo ""
+echo -n "Trim-Galore! version: "
+trim_galore -v
+echo ""
 trim_galore \
 --fastqc \
 -o ${moduledir}/readprocessing/trim_galore \
@@ -89,6 +107,14 @@ source deactivate
 
 # Generate folder with fastqc analysis of raw vs. processed reads
 source activate fastqc
+echo ""
+echo "###################"
+echo "## FastQC"
+echo "###################"
+echo ""
+echo -n "FastQC version: "
+fastqc -v
+echo ""
 fastqc \
 -o ${moduledir}/readprocessing/fastqc/raw/ \
 ${moduledir}/readprocessing/NA_R1.fastq \
@@ -99,6 +125,14 @@ mv ${moduledir}/readprocessing/trim_galore/unfixrm_NA_R2.cor_val_2_fastqc* ${mod
 
 # Assembly with rnaSPAdes and generate assembly summary statistics
 source activate spades
+echo ""
+echo "###################"
+echo "## rnaSPAdes"
+echo "###################"
+echo ""
+echo -n "spades version: "
+spades.py -v
+echo ""
 spades.py \
 --rna \
 --ss rf \
