b767877025c849bbcc6fbd1b56d9d3229d921754
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-03-09 12:11:48 +0100

    Update readprocessing.sh

--- Module_2/readprocessing.sh
+++ Module_2/readprocessing.sh
@@ -14,9 +14,18 @@
 ####################################
 
 # Paths to the experiment file and to the project folder
-srvdir="path/to/reads"
-mydir="wd"
-experiment_file="${mydir}/experiment.txt"
+mydir="/path/to/wd"
+moduledir="${mydir}/Module_2"
+experiment_file="${moduledir}/experiment.txt"
+
+####################################
+#
+# Paths to scripts
+#
+####################################
+
+Rcorrector_dir="/scratch2/software/anaconda/envs/rcorrector/bin/"
+Discard_dir="/scratch4/shess/Jenny/Scripts/"
 
 ####################################
 #
@@ -37,7 +46,7 @@ done < $experiment_file
 # Declare condition arrays
 declare -a cond
 
-# Load experiment file, parse for conditions and save unique conditions into array. Note: Condition1 is always "control"
+# Load experiment file, parse for conditions and save unique conditions into array
 let i=1
 cond[0]="control"
 while read -r f1 f2; do
@@ -106,7 +115,36 @@ if [ ! -d "${moduledir}/readprocessing/fastqc/processed/" ]; then
   mkdir ${moduledir}/readprocessing/fastqc/processed/
 fi
 
-# Start rcorrector
+if [ ! -d "${moduledir}/rRNA/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/rRNA/"
+  mkdir ${moduledir}/rRNA/
+fi
+
+if [ ! -d "${moduledir}/readprocessing/blacklist_rRNA/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/readprocessing/blacklist_rRNA/"
+  mkdir ${moduledir}/readprocessing/blacklist_rRNA/
+fi
+
+if [ ! -d "${moduledir}/NA_ref/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/NA_ref/"
+  mkdir ${moduledir}/NA_ref/
+fi
+
+if [ ! -d "${moduledir}/readprocessing/blacklist_NA/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/readprocessing/blacklist_NA/"
+  mkdir ${moduledir}/readprocessing/blacklist_NA/
+fi
+
+# kmer-based error correction with Rcorrector
+echo ""
+echo "###################"
+echo "## Rcorrector"
+echo "###################"
+echo ""
 
 for i in "${samples[@]}"; do
   mytime=$(date "+%Y-%m-%d %H:%M:%S")
@@ -114,9 +152,128 @@ for i in "${samples[@]}"; do
   echo "$mytime Start rcorrector of sample ${i}"
   echo ""
   
-  perl /scratch2/software/anaconda/envs/rcorrector/bin/run_rcorrector.pl \
-  -p $mydir/rcorrector/${i}_1.fq.gz $mydir/rcorrector/${i}_2.fq.gz \
-  -od $mydir/rcorrector/corrected \
+  perl ${Rcorrector_dir}/run_rcorrector.pl \
+  -p ${moduledir}/readprocessing/${i}_1.fq.gz ${moduledir}/readprocessing/${i}_2.fq.gz \
+  -od ${moduledir}/readprocessing/corrected \
   -t 10
   
 done
+
+# Remove unfixable read pairs, compress output
+echo ""
+echo "###################"
+echo "## Discard"
+echo "###################"
+echo ""
+
+for i in "${samples[@]}"; do
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo ""
+  echo "$mytime Start discarding unfixable read pairs of sample ${i}"
+  echo ""
+  
+  python ${Discard_dir}/FilterUncorrectabledPEfastq.py \
+  -1 ${moduledir}/readprocessing/corrected/${i}_1.cor.fq.gz \
+  -2 ${moduledir}/readprocessing/corrected/${i}_2.cor.fq.gz \
+  -s ${i}
+  pigz ${moduledir}/readprocessing/corrected/unfixrm_${i}_1.cor.fq
+  pigz ${moduledir}/readprocessing/corrected/unfixrm_${i}_2.cor.fq
+  
+done
+
+# Perform quality and adapter trimming with Trim-Galore!
+source activate trim-galore
+echo ""
+echo "###################"
+echo "## Trim-Galore!"
+echo "###################"
+echo ""
+echo -n "Trim-Galore! version: "
+trim_galore -v
+echo ""
+
+for i in "${samples[@]}"; do
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo ""
+  echo "$mytime Start trimming of sample ${i}"
+  echo ""
+  
+  trim_galore \
+  --fastqc \
+  -o ${moduledir}/readprocessing/trim_galore \
+  -j 4 \
+  --paired \
+  --length 49 \
+  ${moduledir}/readprocessing/corrected/unfixrm_${i}_1.cor.fq.gz \
+  ${moduledir}/readprocessing/corrected/unfixrm_${i}_2.cor.fq.gz
+  
+done
+source deactivate
+
+# Generate folder with fastqc analysis of raw vs. processed reads
+source activate fastqc
+echo ""
+echo "###################"
+echo "## FastQC"
+echo "###################"
+echo ""
+echo -n "FastQC version: "
+fastqc -v
+echo ""
+
+for i in "${samples[@]}"; do
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo ""
+  echo "$mytime Start fastqc analysis of sample ${i}"
+  echo ""
+  
+  fastqc \
+  -o ${moduledir}/readprocessing/fastqc/raw/ \
+  ${moduledir}/readprocessing/${i}_1.fq.gz \
+  ${moduledir}/readprocessing/${i}_2.fq.gz
+  mv ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_R1.cor_val_1_fastqc* ${moduledir}/readprocessing/fastqc/processed/
+  mv ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_R2.cor_val_2_fastqc* ${moduledir}/readprocessing/fastqc/processed/
+  
+done
+source deactivate
+
+# Map reads to Mougeotia and Orciraptor known rRNA sequences, sort in aligned and unaligned
+# Create bowtie2 index for fasta file containing Mougeotia and Orciraptor rRNA sequences
+source activate bowtie2
+bowtie2-build ${moduledir}/ssu_T.fasta ${moduledir}/rRNA/ssu_T.fasta
+
+echo ""
+echo "###################"
+echo "## rRNA mapping bowtie2"
+echo "###################"
+echo ""
+echo -n "bowtie2 version: "
+fastqc -v
+echo ""
+
+for i in "${samples[@]}"; do
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo ""
+  echo "$mytime Start rRNA mapping of sample ${i}"
+  echo ""
+  
+  bowtie2 \
+  -p 10 \
+  --very-sensitive \
+  --score-min C,0,0 \
+  --phred33 \
+  --al-conc-gz ${moduledir}/readprocessing/blacklist_rRNA/blacklist_rRNA_paired_aligned_${i}.fq.gz \
+  --un-conc-gz ${moduledir}/readprocessing/blacklist_rRNA/blacklist_rRNA_paired_unaligned_${i}.fq.gz \
+  -x ${moduledir}/rRNA/ssu_T.fasta \
+  -1 ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_1.cor_val_1.fq.gz \
+  -2 ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_2.cor_val_2.fq.gz
+  
+done
+
+# Map reads to Mougeotia transcriptome created in Module_1
+# Create bowtie2 index for Mougeotia transcriptome
+
+bowtie2-build ${mydir}/Module_1/NA_rnaspades/transcripts.fasta ${moduledir}/NA_ref/NA_ref.fasta
+
+
+  
