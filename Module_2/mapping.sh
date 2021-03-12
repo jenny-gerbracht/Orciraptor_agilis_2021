fabe02eef8d81d498a188884f363a8872d9c2604
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-03-12 09:35:36 +0100

    Update mapping.sh

--- Module_2/mapping.sh
+++ Module_2/mapping.sh
@@ -4,6 +4,9 @@
 #$ -cwd
 #$ -pe threaded 10
 
+# Performs the following steps: d) read mapping and sorting to rRNA sequences of Orciraptor agilis and Mougeotia sp., 
+# e) read mapping and sorting to transcriptome of Mougeotia sp.
+
 ####################################
 #
 # Paths to reads and experiment file
@@ -14,15 +17,6 @@
 mydir="/path/to/wd"
 moduledir="${mydir}/Module_2"
 experiment_file="${moduledir}/experiment.txt"
-
-####################################
-#
-# Paths to scripts
-#
-####################################
-
-Rcorrector_dir="/scratch2/software/anaconda/envs/rcorrector/bin/"
-Discard_dir="/scratch4/shess/Jenny/Scripts/"
 
 ####################################
 #
@@ -82,61 +76,6 @@ for i in $( eval echo {0..${arr_length}} );do
   echo -e "cond${i} \t ${cond[i]} \t $(eval echo \${cond$i[*]})"
 done
 
-####################################
-#
-# Make folders
-#
-####################################
-
-if [ ! -d "${moduledir}/readprocessing/corrected/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/corrected/"
-  mkdir ${moduledir}/readprocessing/corrected/
-fi
-
-if [ ! -d "${moduledir}/readprocessing/fastqc/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/fastqc/"
-  mkdir ${moduledir}/readprocessing/fastqc/
-fi
-
-if [ ! -d "${moduledir}/readprocessing/fastqc/raw/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/fastqc/raw/"
-  mkdir ${moduledir}/readprocessing/fastqc/raw/
-fi
-
-if [ ! -d "${moduledir}/readprocessing/fastqc/processed/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/fastqc/processed"
-  mkdir ${moduledir}/readprocessing/fastqc/processed/
-fi
-
-if [ ! -d "${moduledir}/rRNA/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/rRNA/"
-  mkdir ${moduledir}/rRNA/
-fi
-
-if [ ! -d "${moduledir}/readprocessing/blacklist_rRNA/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/blacklist_rRNA/"
-  mkdir ${moduledir}/readprocessing/blacklist_rRNA/
-fi
-
-if [ ! -d "${moduledir}/NA_ref/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/NA_ref/"
-  mkdir ${moduledir}/NA_ref/
-fi
-
-if [ ! -d "${moduledir}/readprocessing/blacklist_NA/" ]; then
-  mytime=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$mytime Make directory ${moduledir}/readprocessing/blacklist_NA/"
-  mkdir ${moduledir}/readprocessing/blacklist_NA/
-fi
-
-
 # Map reads to Mougeotia and Orciraptor known rRNA sequences, sort in aligned and unaligned
 # Create bowtie2 index for fasta file containing Mougeotia and Orciraptor rRNA sequences from SILVA SSU r138.1 database: sequences from groups "Orciraptor" and "Mougeotia",
 # U replaced with T
@@ -169,7 +108,7 @@ for i in "${samples[@]}"; do
   -x ${moduledir}/rRNA/ssu_T.fasta \
   -1 ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_1.cor_val_1.fq.gz \
   -2 ${moduledir}/readprocessing/trim_galore/unfixrm_${i}_2.cor_val_2.fq.gz \
-  --met-file bowtie2_alignment-metrics.txt | samtools view -bS - > rRNA.BAM ) 3>&1 1>&2 2>&3 | tee rRNA.stderr.log 
+  --met-file bowtie2_alignment-metrics_rRNA.txt | samtools view -bS - > rRNA.BAM ) 3>&1 1>&2 2>&3 | tee rRNA.stderr.log 
   rm rRNA.BAM
   
 done
@@ -202,7 +141,7 @@ for i in "${samples[@]}"; do
   -x ${moduledir}/NA_ref/NA_ref.fasta \
   -1 ${moduledir}/readprocessing/blacklist_rRNA/blacklist_rRNA_paired_unaligned_${i}.fq.1.gz \
   -2 ${moduledir}/readprocessing/blacklist_rRNA/blacklist_rRNA_paired_unaligned_${i}.fq.2.gz \
-  --met-file bowtie2_alignment-metrics.txt | samtools view -bS - > mougeotia.BAM ) 3>&1 1>&2 2>&3 | tee mougeotia.stderr.log 
+  --met-file bowtie2_alignment-metrics_mougeotia.txt | samtools view -bS - > mougeotia.BAM ) 3>&1 1>&2 2>&3 | tee mougeotia.stderr.log 
   rm mougeotia.BAM
   
 done
