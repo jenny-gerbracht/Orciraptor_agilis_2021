64a120dd56f5f4d10267f2b903a57336309174c8
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-04-07 12:15:56 +0200

    Update transdecoder.sh

--- Module_3/transdecoder.sh
+++ Module_3/transdecoder.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#$ -S /bin/bash
+. /etc/profile
+#$ -cwd
 
+# Define paths to read and working directory locations
 moduledir="${mydir}/Module_3"
 
+# Log file
+# Get date
+date=$(date "+%Y-%m-%d")
+
+# Define log file and redirect stdout and stderr to this file
+if [ ! -d "${moduledir}/Logs/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/Logs/"
+  mkdir ${moduledir}/Logs/
+fi
+log_file="${moduledir}/Logs/log_transdecoder_$date"
+exec &> >(tee -a "$log_file")
+
+# Create necessary folders
 if [ ! -d "${moduledir}/transdecoder/" ]; then
   mytime=$(date "+%Y-%m-%d %H:%M:%S")
   echo "$mytime Make directory ${moduledir}/transdecoder/"
@@ -10,5 +28,9 @@ fi
 
 cd transdecoder
 
+source activate transdecoder
+
 TransDecoder.LongOrfs -t ../orciraptor_200_filtered.fasta
 TransDecoder.Predict -t ../orciraptor_200_filtered.fasta
+
+source deactivate
