3c2522b84bb26c1cb60478d2d27f18f2bd5fd187
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-04-07 12:10:56 +0200

    Update transdecoder.sh

--- Module_3/transdecoder.sh
+++ Module_3/transdecoder.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mkdir transdecoder
+moduledir="${mydir}/Module_3"
+
+if [ ! -d "${moduledir}/transdecoder/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/transdecoder/"
+  mkdir ${moduledir}/transdecoder/
+fi
+
 cd transdecoder
 
 TransDecoder.LongOrfs -t ../orciraptor_200_filtered.fasta
