131ce52fce74dd27f159aa7e27e04d2a0d0e4e6a
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-04-08 16:14:13 +0200

    Create transdecoder.sh

---
+++ Module_1/transdecoder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#$ -S /bin/bash
+. /etc/profile
+#$ -cwd
+
+# Define paths to read and working directory locations
+mydir="/path/to/wd"
+moduledir="${mydir}/Module_1"
+
+# Log file
+# Get date
+date=$(date "+%Y-%m-%d")
+
+# Define log file and redirect stdout and stderr to this file
+if [ ! -d "${moduledir}/Logs/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/Logs/"
+  mkdir ${moduledir}/Logs/
+fi
+log_file="${moduledir}/Logs/log_transdecoder_$date"
+exec &> >(tee -a "$log_file")
+
+# Create necessary folders
+if [ ! -d "${moduledir}/transdecoder/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$mytime Make directory ${moduledir}/transdecoder/"
+  mkdir ${moduledir}/transdecoder/
+fi
+
+cd transdecoder
+
+source activate transdecoder
+
+TransDecoder.LongOrfs -t ${moduledir}/NA_transcripts.fasta
+TransDecoder.Predict -t ${moduledir}/NA_transcripts.fasta
+
+source deactivate
