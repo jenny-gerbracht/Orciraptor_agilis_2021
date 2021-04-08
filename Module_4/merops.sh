adb6fe62d63ad4da7a3fe98a6c6334e80ed8d349
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-04-08 12:31:43 +0200

    Create merops.sh

---
+++ Module_4/merops.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define paths to working directory locations
+mydir="/path/to/wd"
+moduledir="${mydir}/Module_4"
+
+# Log file
+# Get date
+date=$(date "+%Y-%m-%d")
+
+# Define log file and redirect stdout and stderr to this file
+if [ ! -d "${moduledir}/Logs/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "${mytime} Make directory ${moduledir}/Logs/"
+  mkdir ${moduledir}/Logs/
+fi
+log_file="${moduledir}/Logs/log_merops_$date"
+exec &> >(tee -a "$log_file")
+
+# Create necessary folders
+if [ ! -d "${moduledir}/merops/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "${mytime} Make directory ${moduledir}/merops/"
+  mkdir ${moduledir}/merops/
+fi
+
+# Annotation of peptidases with MEROPS database
+echo ""
+echo "###################"
+echo "## MEROPS"
+echo "###################"
+echo ""
+
+diamond \
+blastp \
+--sensitive \
+--query ${mydir}/Module_3/transdecoder/orciraptor_transdecoder.pep_renamed.fasta \
+--threads 10 \
+--out ${moduledir}/merops/merops.out \
+--db /srv/Jenny/MEROPS/merops.dmnd \
+-outfmt 6 qseqid sseqid stitle pident length mismatch gapopen qstart qend sstart send evalue bitscore \
+--max-target-seqs 1
+
+
+
+
