67b875c7db923890c1a7147e6c8103c8bedf7635
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-04-08 16:41:24 +0200

    Update seqkit_NA.sh

--- Module_3/seqkit_NA.sh
+++ Module_3/seqkit_NA.sh
@@ -4,5 +4,5 @@ seqkit \
 grep \
 -n -f contaminants_NA.txt \
 ./transdecoder/orciraptor_200_filtered.fasta.transdecoder.pep \
--o /transdecoder/orciraptor_200_filtered2.fasta.transdecoder.pep \
+-o ./transdecoder/orciraptor_200_filtered2.fasta.transdecoder.pep \
 -v
