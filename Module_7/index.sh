98e3b310af42e487543e92f0cf7f900880a03d2c
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-05-03 11:15:27 +0200

    Create index.sh

---
+++ Module_7/index.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Define paths to working directory locations
+mydir="/path/to/wd"
+moduledir="${mydir}/Module_7"
+experiment_file="${moduledir}/experiment.txt"
+
+# Log file
+# Get date
+date=$(date "+%Y-%m-%d")
+
+# Define log file and redirect stdout and stderr to this file
+if [ ! -d "${moduledir}/Logs/" ]; then
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "${mytime} Make directory ${moduledir}/Logs/"
+  mkdir ${moduledir}/Logs/
+fi
+log_file="${moduledir}/Logs/log_mapping_$date"
+exec &> >(tee -a "$log_file")
+
+####################################
+#
+# Setting up samples array
+#
+####################################
+
+# Declare samples array
+declare -a samples
+
+# Load experiment file, parse for samples and save them into the array
+let p=0
+while read -r f1 f2; do
+  samples[p]="${f1}"
+  ((++p))
+done < $experiment_file
+
+# Declare condition arrays
+declare -a cond
+
+# Load experiment file, parse for conditions and save unique conditions into array
+let i=0
+while read -r f1 f2; do
+  if [[ " ${cond[*]} " == *"$f2"* ]];then
+  continue
+else
+  cond[i]="${f2}"
+  ((++i))
+fi
+done < $experiment_file
+
+# Declare individual condition arrays
+arr_length="$((${#cond[@]}-1))"
+for i in $( eval echo {0..${arr_length}} );do
+  declare -a cond${i}
+done
+
+# Load experiment file again, parse for conditions and save filenames into condition-specific arrays.
+while read -r f1 f2; do
+  for i in $( eval echo {0..${arr_length}} );do
+  if [[ "$f2" == "${cond[i]}" ]];then
+    eval cond${i}[cond${i}count]="${f1}"
+    ((++cond${i}count))
+  fi
+done
+done < $experiment_file
+
+# State the conditions and samples for this analysis
+echo "############################"
+echo "## Conditions and samples ##"
+echo "############################"
+echo ""
+
+arr_length="$((${#cond[@]}-1))"
+for i in $( eval echo {0..${arr_length}} );do
+  echo -e "cond${i} \t ${cond[i]} \t $(eval echo \${cond$i[*]})"
+done
+
+echo ""
+echo "###################"
+echo "## Indexing"
+echo "###################"
+echo ""
+
+for i in "${samples[@]}"; do
+  mytime=$(date "+%Y-%m-%d %H:%M:%S")
+  echo ""
+  echo "$mytime Start STAR mapping of sample ${i}"
+  echo ""
+
+  samtools index ${i}Aligned.sortedByCoord.out.bam
+
+done
