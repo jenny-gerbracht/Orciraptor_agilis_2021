637091b1c3278601ffaea450f7af081a9503fd50
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-10-11 13:38:58 +0200

    Update seqkit_length.sh

--- Module_3/seqkit_length.sh
+++ Module_3/seqkit_length.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+###################################################################
+#Script Name	:seqkit_length.sh		                                                                                              
+#Description	:Removes contigs from fasta that are smaller than 200 nt                                                                      
+#Args:                                                                                           
+#Author	:Jennifer Gerbracht                                               
+#Email		:jennifer.gerbracht@gmx.de                                           
+###################################################################
+
 seqkit \
 seq \
 -m 200 \
