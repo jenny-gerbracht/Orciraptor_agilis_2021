3f816589cdfb060f2577f9adba217aec3f1e7ed6
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-10-11 13:38:31 +0200

    Update seqkit_NA.sh

--- Module_3/seqkit_NA.sh
+++ Module_3/seqkit_NA.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+###################################################################
+#Script Name	:seqkit_NA.sh		                                                                                              
+#Description	:Removes ORFs that were identified in blastp search                                                                              
+#Args:                                                                                           
+#Author	:Jennifer Gerbracht                                               
+#Email		:jennifer.gerbracht@gmx.de                                           
+###################################################################
+
 seqkit \
 grep \
 -n -f contaminants_NA.txt \
