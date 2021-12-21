668dd074ef48077e35fe135c87907ab62abdea69
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-12-21 12:33:19 +0100

    Update ExN50.sh

--- Module_6/ExN50.sh
+++ Module_6/ExN50.sh
@@ -15,6 +15,8 @@ source ../config.txt
 
 ${Trinity_dir}/util/abundance_estimates_to_matrix.pl \
 --est_method salmon \
+--gene_trans_map none \
+--name_sample_by_basedir \
 ${mydir}/Module_5/salmon/V1S1.salmon_quant/quant.sf \
 ${mydir}/Module_5/salmon/V1S2.salmon_quant/quant.sf \
 ${mydir}/Module_5/salmon/V1S3.salmon_quant/quant.sf \
@@ -24,3 +26,11 @@ ${mydir}/Module_5/salmon/V1S6.salmon_quant/quant.sf \
 ${mydir}/Module_5/salmon/V1S7.salmon_quant/quant.sf \
 ${mydir}/Module_5/salmon/V1S8.salmon_quant/quant.sf \
 ${mydir}/Module_5/salmon/V1S9.salmon_quant/quant.sf \
+
+${Trinity_dir}/util/misc/contig_ExN50_statistic.pl \
+salmon.isoform.TMM.EXPR.matrix \
+${mydir}/Module_3/orciraptor_200_filtered.fasta \
+| tee ExN50.stats
+
+${Trinity_dir}/util/misc/plot_ExN50_statistic.Rscript \
+ExN50.stats 
